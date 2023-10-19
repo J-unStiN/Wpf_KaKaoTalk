93377e2e23d8b9d5e035f979be027ae03501371e
sj <sj@JUNSTIN06AC>
2023-10-20 01:21:10 +0900

    초기 커밋.

---
+++ Wpf_KaKaoTalk/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf_KaKaoTalk.Service;
+
+namespace Wpf_KaKaoTalk
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainWindow>();
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+
+        public IServiceProvider Services { get; }
+
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ITestService, TestService>();
+
+
+            services.AddSingleton<MainWindow>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Wpf_KaKaoTalk/Service/TestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_KaKaoTalk.Service
+{
+    public class TestService : ITestService
+    {
+        public string Getstring()
+        {
+            return "test service";
+        }
+
+    }
+}
